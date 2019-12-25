03ef0620e653db8fcb0e9ff3cbb015e6520a7412
Baptiste Zammit <baptiste.zammit@epita.fr>
2019-12-25 16:07:49 +0100

    Pixel art fixed

--- Decoding/binary_to_string.c
+++ Decoding/binary_to_string.c
@@ -3,8 +3,8 @@
 int binary_to_string(char *binary)
 {
     char letter = strtol(binary, 0, 2);
-  //  if (letter < 0 || letter > 255)
-    //    return 1;
+    if (letter < 0 || letter > 255)
+        return 1;
     printf("%c", letter);
     return 0;
 }
