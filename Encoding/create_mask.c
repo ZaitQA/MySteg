134efc7cd4b2bf0d24d96eb47c04343fb1227905
Jeremie Zeitoun <jeremie.zeitoun@epita.fr>
2019-12-25 16:15:32 +0100

    Add create mask

---
+++ Encoding/create_mask.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+char bit_reverse(char c)
+{
+    if (c == '0')
+        return '1';
+    return '0';
+}
+
+char *create_mask(char *bits, char *message, size_t len)
+{
+    char *mask = calloc(sizeof(char), len + 2);
+    for (size_t i = 0; i < len; i++)
+    {
+        if (message[i] == '0')
+            mask[i] = bits[i];
+        if (message[i] == '1')
+            mask[i] = bit_reverse(bits[i]);
+    }
+    return mask;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+        return 1;
+    char *s = create_mask(argv[1], argv[2], strlen(argv[1]));
+    printf("%s\n", s);
+    free(s);
+    return 0;
+}
---
+++ steg.d
@@ -0,0 +1,3 @@
+steg: main.c Encoding/string_to_binary.h Display/display.h \
+ Encoding/Treatment_encoding.h Decoding/Treatment_decoding.h \
+ Display/save.h /usr/include/SDL/SDL.h Display/../Tools/IMG_savepng.h
